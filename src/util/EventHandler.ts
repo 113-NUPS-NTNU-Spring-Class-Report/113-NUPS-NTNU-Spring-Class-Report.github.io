f22a9e59ef301e5f8ffdbef100f91455933cacdc
阿循 <105115552+GANSOONLEE@users.noreply.github.com>
2024-03-18 01:50:05 +0000

    commit

--- src/util/EventHandler.ts
+++ src/util/EventHandler.ts
@@ -1,7 +1,33 @@
 
+
 export class EventHandler
 {
 
-    public static eventArray: Array<Function> = [];
+    public eventArray: Array<any> = [];
+
+    constructor(eventFunction: Function, delay: number)
+    {
+        const event = {
+            function: eventFunction,
+            delay: delay,
+        }; 
+        this.eventArray.push(event);
+    }
+
+    /**
+     * Start the event process
+     */
+    public run ()
+    {
+        this.eventArray.forEach( (event, index) => {
+
+            let delay = 
+
+            setTimeout( () => {
+                event.function();
+            }, )
+
+        });
+    }
 
 }
