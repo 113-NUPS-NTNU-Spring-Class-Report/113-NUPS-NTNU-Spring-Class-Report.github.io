3abb2d0f1adbe5e88d82408c19af44ce98cf72af
阿循 <105115552+GANSOONLEE@users.noreply.github.com>
2024-04-22 01:11:00 +0000

    :wrench: update content

--- src/chapter/first.ts
+++ src/chapter/first.ts
@@ -12,6 +12,45 @@ guiManagement.toggleOptions();
 
 const eventHandle = new EventHandle();
 
+// branch 1
+const branch1 = new EventHandle();
+
+branch1.addAction(() => {
+    guiManagement.toggleOptions();
+    guiManagement.setContent("這裏到底是。。。？");
+}, 0);
+
+branch1.addAction(() => {
+    guiManagement.setContent("我的身體在冰冷的地面上，再仔細地感受一下，地面上伴隨著逐漸變的强烈的震動。");
+}, 4000);
+
+branch1.addAction(() => {
+    guiManagement.setOptions(
+        [
+            {
+                text: '地面的震動我無法忽視，我的理智告訴我必須用盡全力起身。',
+                event: () => {
+                    
+                }
+            },
+            {
+                text: '我的身體好痛，肋骨好像刺穿了我的肺部。要不。。。就別起了。就這樣死去，也沒事吧。。。',
+                event: () => {
+                    guiManagement.toggleOptions();
+                    branchTwo();
+                }
+            },
+        ]
+    );
+    guiManagement.toggleOptions();
+}, 4000);
+// branch 2
+const branch2 = new EventHandle();
+
+branch2.addAction(() => {
+    guiManagement.setContent("此時，大腦傳來陣痛感。一些零碎畫面逐漸在我腦海中浮現。");
+}, 2000);
+
 // Add action
 eventHandle.addAction(() => {
     guiManagement.setTitle('Chp 1');
@@ -20,11 +59,11 @@ eventHandle.addAction(() => {
 eventHandle.addAction(() => {
     guiManagement.toggleDialog();
     guiManagement.setContent('醒醒！快醒醒！');
-}, 1000);
+}, 6000);
 
 eventHandle.addAction(() => {
     guiManagement.setContent('我感受到一股力量在搖晃我的身體，耳朵也不時傳來焦慮的低語聲。');
-}, 4000);
+}, 1000);
 
 eventHandle.addAction(() => {
     guiManagement.setContent('我的眼前一片黑暗，而空氣中。。。似乎還迷茫著血腥味。。。我的大腦一片模糊，但我的第六感告訴我："我必須馬上起身！"');
@@ -37,8 +76,7 @@ eventHandle.addAction(() => {
             {
                 text: '搖晃我的是誰？不對！我應該在等等，先裝死。',
                 event: () => {
-                    branchOne();
-                    // guiManagement.toggleOptions();
+                    branch1.start();
                 }
             },
             {
@@ -77,7 +115,7 @@ eventHandle.addAction(() => {
                         text: '我的身體好痛，肋骨好像刺穿了我的肺部。要不。。。就別起了。就這樣死去，也沒事吧。。。',
                         event: () => {
                             guiManagement.toggleOptions();
-                            branchTwo();
+                            branch2.start();
                         }
                     },
                 ]
@@ -89,9 +127,7 @@ eventHandle.addAction(() => {
     // Branch two
     function branchTwo()
     {
-        eventHandle.addAction(() => {
-            guiManagement.setContent("此時，大腦傳來陣痛感。一些零碎畫面逐漸在我腦海中浮現。");
-        }, 2000);
+        
     }
 
 // run event serial
